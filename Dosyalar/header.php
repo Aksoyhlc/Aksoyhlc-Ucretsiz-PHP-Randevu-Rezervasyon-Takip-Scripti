7dc2772c47eee50d8ad19e240e5b7dddc6843113
Ökkeş Aksoy <27aksoy27@gmail.com>
2021-12-30 12:24:26 +0300

    Update header.php

--- Dosyalar/header.php
+++ Dosyalar/header.php
@@ -52,7 +52,7 @@ oturum();
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
                 <div class="sidebar-brand-text mx-3">AKSOYHLC</div>
             </a>
 
